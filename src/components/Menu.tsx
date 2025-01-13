ca007392db888e95e6ab1c50269ddd31b9426d75
majidhussain1op <142897507+majidhussain1op@users.noreply.github.com>
2025-01-13 15:16:46 +0500

    Add files via upload

---
+++ src/components/Menu.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {useState} from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+const Menu = () => {
+  const [open, setOpen]=useState(false)
+  return(
+    <div>
+      <Image src="/menu.png" alt="menu" width={28} height={28} className="cursor-pointer" onClick={() => setOpen((prev) =>!prev)}/>
+      {
+        open && (
+          <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80)] flex flex-col items-center justify-center gap-8 text-xl z-10">
+              <Link href="/">Home</Link>
+              <Link href="/">Shop</Link>
+              <Link href="/">Deals</Link>
+              <Link href="/">About</Link>
+              <Link href="/">Contact</Link>
+              <Link href="/">Logout</Link>
+              <Link href="/">Cart</Link>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default Menu;
